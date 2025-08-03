2bc2722b4202aaa9f4f299d9e436b611c9aec227
Rahul Teggi <rahulteggi17@gmail.com>
2025-08-03 15:17:46 -0400

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
